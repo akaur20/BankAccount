f364c29884ccf2a880e411a5e7b00bf16234dead
akaur20 <42978513+akaur20@users.noreply.github.com>
2018-12-10 14:29:37 -0500

    Add files via upload

---
+++ src/BankAccount.java
@@ -0,0 +1,18 @@
+
+public abstract class BankAccount 
+{
+	private static int nextAccNum;
+	private String name;
+	private int accNum;
+	private double balance;
+	
+	BankAccount(String n)
+	{
+		
+	}
+	
+	BankAccount(String n, double b)
+	{
+		
+	}
+}
